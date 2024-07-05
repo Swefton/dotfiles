dd90ca07d128244c332f98d69983e14295cadeeb
amrit <amrit.madhav.srivastava@gmail.com>
2024-07-05 17:24:08 +0000

    initial switch to hyprland

--- sway/todocked.sh
+++
@@ -1,7 +0,0 @@
-if [ "$(swaymsg -t get_outputs | jq '. | length')" -eq 1 ]; then
-    systemctl suspend
-else
-    wlr-randr --output eDP-1 --off
-    killall waybar
-    waybar -c ~/.config/waybar/docked
-fi
